c78b64d12a11dc2755b0bf6fbd36a105c3e739a3
BethDianeSagarino <bethsagarino@gmail.com>
2022-06-10 21:56:07 +0800

    Android Studio to Firebase

---
+++ app/src/main/java/com/example/diane_eldroid/Foods.java
@@ -0,0 +1,50 @@
+package com.example.diane_eldroid;
+
+public class Foods {
+    String image, id, name, desc;
+
+    public Foods() {
+
+    }
+
+    public Foods(String image, String id, String name, String desc) {
+        this.image = image;
+        this.id = id;
+        this.name= name;
+        this.desc = desc;
+
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+}
